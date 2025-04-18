351dffb057e905e35a733a35eb3276c3cd6d65d8
Tom Nehil <tom.nehil@gmail.com>
2025-04-18 15:57:04 -0500

    Adds min player count, max player count and recommended player count. Also code formatting

--- lambda_function.py
+++ lambda_function.py
@@ -14,36 +14,44 @@ AIRTABLE_BASE = "appP335Rk2WlkFqCs"
 COLLECTION_TABLE = "tblFOoQ5UokcjjqNB"
 PLAYS_TABLE = "tblWr6IW1xFO1ni3b"
 
+
 class CollectionNotReadyException(BaseException):
     pass
 
+
 class BGGSession(requests.Session):
     """An authenticated BGG session"""
+
     def __init__(self, user=USER, pw=PW):
         self.user = user
         super().__init__()
-        self.post("https://boardgamegeek.com/login/api/v1", json={"credentials": {"username": user, "password": pw}})
+        self.post(
+            "https://boardgamegeek.com/login/api/v1",
+            json={"credentials": {"username": user, "password": pw}},
+        )
 
     def get_collection(self):
         """Requests BGG user collection with private items; returns BS4 soup"""
-        req = self.get(f"https://boardgamegeek.com/xmlapi2/collection?username={self.user}&showprivate=1")
+        req = self.get(
+            f"https://boardgamegeek.com/xmlapi2/collection?username={self.user}&showprivate=1"
+        )
         soup = BeautifulSoup(req.text, features="xml")
         if soup.find("message"):
             raise CollectionNotReadyException
         else:
             return soup
-    
+
     # def get_bgg_plays(page=1):
     #     with  requests.Session() as s:
     #         req = s.get(f"https://boardgamegeek.com/xmlapi2/plays?username={self.user}&page={page}")
     #         return req.text
+
 
 class BGGCollection:
     def __init__(self, raw_data):
         self.data = self.read_bgg_collection(raw_data)
 
     def read_bgg_collection(self, raw_data):
-
         items = raw_data.find_all("item")
 
         game_data = []
@@ -52,7 +60,9 @@ class BGGCollection:
             name = item.find("name").text
             object_id = item.get("objectid")
             coll_id = item.get("collid")
-            pub_year = item.find("yearpublished").text if item.find("yearpublished") else ""
+            pub_year = (
+                item.find("yearpublished").text if item.find("yearpublished") else ""
+            )
             status = []
             statuses = item.find("status")
             if statuses.get("own") == "1":
@@ -66,31 +76,40 @@ class BGGCollection:
             acquisition_date = private.get("acquisitiondate") if private else ""
             price_paid = private.get("pricepaid") if private else ""
             acquired_from = private.get("acquiredfrom") if private else ""
-            game_data.append({
-                "fields": {
-                    "game": name,
-                    "object_id": object_id,
-                    "coll_id": coll_id,
-                    "pub_year": int(pub_year) if pub_year else "",
-                    "status": status,
-                    "plays": int(plays) if plays else "",
-                    "date_acquired": acquisition_date,
-                    "year_acquired": acquisition_date.split("-")[0] if acquisition_date else "",
-                    "price_paid": float(price_paid) if price_paid else "",
-                    "acquired_from": acquired_from,
-                    "weight": 0.0,
-                    "designers": []
+            game_data.append(
+                {
+                    "fields": {
+                        "game": name,
+                        "object_id": object_id,
+                        "coll_id": coll_id,
+                        "pub_year": int(pub_year) if pub_year else "",
+                        "status": status,
+                        "plays": int(plays) if plays else "",
+                        "date_acquired": acquisition_date,
+                        "year_acquired": acquisition_date.split("-")[0]
+                        if acquisition_date
+                        else "",
+                        "price_paid": float(price_paid) if price_paid else "",
+                        "acquired_from": acquired_from,
+                        "weight": 0.0,
+                        "min_players": 0,
+                        "max_players": 0,
+                        "recommended_players": "",
+                        "designers": [],
+                    }
                 }
-            })
+            )
 
         self.data = game_data
         self.set_game_specific_data()
         return self.data
-    
+
     def set_game_specific_data(self):
         ids = [game["fields"]["object_id"] for game in self.data]
         for batch in batched(ids, 20):
-            url = f"https://boardgamegeek.com/xmlapi2/thing?stats=1&id={','.join(batch)}"
+            url = (
+                f"https://boardgamegeek.com/xmlapi2/thing?stats=1&id={','.join(batch)}"
+            )
             print(f"Fetching {url}...")
             req = requests.get(url)
             lookup = {}
@@ -99,18 +118,46 @@ class BGGCollection:
             for item in items:
                 id = item.get("id")
                 weight = item.find("statistics").find("averageweight").get("value")
-                designers = [lnk.get("value") for lnk in item.find_all("link") if lnk.get("type") == "boardgamedesigner"]
+                min_players = item.find("minplayers").get("value")
+                max_players = item.find("maxplayers").get("value")
+                poll_summaries = item.find_all("poll-summary")
+                suggest_numplayers_summary = [
+                    summary
+                    for summary in poll_summaries
+                    if summary.get("name") == "suggested_numplayers"
+                ][0]
+                best_with_result = [
+                    res
+                    for res in suggest_numplayers_summary.find_all("result")
+                    if res.get("name") == "bestwith"
+                ][0].get("value")
+
+                designers = [
+                    lnk.get("value")
+                    for lnk in item.find_all("link")
+                    if lnk.get("type") == "boardgamedesigner"
+                ]
                 pub_year = item.find("yearpublished").get("value")
-                lookup[id] = {"weight": weight, "designers": designers, "pub_year": pub_year}
+                lookup[id] = {
+                    "weight": weight,
+                    "designers": designers,
+                    "pub_year": pub_year,
+                    "min_players": min_players,
+                    "max_players": max_players,
+                    "recommended_players": best_with_result,
+                }
             for game in self.data:
                 gid = game["fields"]["object_id"]
                 if gid in lookup:
                     game["fields"]["weight"] = lookup[gid]["weight"]
                     game["fields"]["designers"] = lookup[gid]["designers"]
                     game["fields"]["pub_year"] = lookup[gid]["pub_year"]
+                    game["fields"]["min_players"] = lookup[gid]["min_players"]
+                    game["fields"]["max_players"] = lookup[gid]["max_players"]
+                    game["fields"]["recommended_players"] = lookup[gid][
+                        "recommended_players"
+                    ]
 
-
-        
 
 # def read_bgg_plays(data):
 #     soup = BeautifulSoup(data, features="xml")
@@ -135,11 +182,12 @@ class BGGCollection:
 #                 "plays": qty,
 #                 "location": location,
 #                 "players": players,
-#                 "id": id 
-#             }   
+#                 "id": id
+#             }
 #         })
 
 #     return play_data
+
 
 def update_airtable(game_data, table_id, key_fields):
     api = Api(api_key=AIRTABLE_TOKEN)
@@ -156,22 +204,15 @@ if __name__ == "__main__":
             data = bgg.get_collection()
             break
         except CollectionNotReadyException:
-            print(f"Collection not ready; trying again in 30 seconds. (Attempt #{retries})")
+            print(
+                f"Collection not ready; trying again in 30 seconds. (Attempt #{retries})"
+            )
             time.sleep(30)
-            retries +=1
+            retries += 1
     if data.find("message"):
         print("The collection wasn't successfully retrieved.")
     else:
         game_data = BGGCollection(data)
-        update_airtable(game_data.data, COLLECTION_TABLE, ["game", "object_id", "coll_id"])
-    
-    # soup = BeautifulSoup(data, features="xml")
-    # if soup.find("message"):
-    #     print("Request initiated. Waiting 30 seconds.")
-    #     time.sleep(30)
-    #     data = cnx.get_collection()
-    #     print(data)
-    # collex = BGGCollection()
-    # game_data = collex.read_bgg_collection(soup)
-    # update_airtable(game_data, COLLECTION_TABLE, ["game", "object_id", "coll_id"])
-    
+        update_airtable(
+            game_data.data, COLLECTION_TABLE, ["game", "object_id", "coll_id"]
+        )
